5cc9af882ce448b3bea427f9cd1a095bb6191ee3
eshalev <eshalev@cisco.com>
2017-01-21 00:53:03 +0100

    Added pirate ship

---
+++ Assets/Eyal/assets/pirateBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class pirateBehaviour : MonoBehaviour {
+
+    public  GameObject m_Target;
+    NavMeshAgent m_NavMeshAgent;
+
+    // Use this for initialization
+    void Start () {
+         m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        m_NavMeshAgent.SetDestination(m_Target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
