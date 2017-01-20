269044d070c92f7d53727c0ba23e91d05c24ff2d
eshalev <eshalev@cisco.com>
2017-01-20 21:54:31 +0100

    added explosions

--- Assets/Eyal/assets/BombBehaviour.cs
+++ Assets/Eyal/assets/BombBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BombBehaviour : MonoBehaviour {
-   
+    public ParticleSystem m_Explosion;
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,7 +14,9 @@ public class BombBehaviour : MonoBehaviour {
     {
         //check if colliding with a boat
         var boat = collision.collider.GetComponent<BoatResponseBehaviour>();
-        Debug.Log("Bomb!");
+        Debug.Log("Boom!");
+        m_Explosion.Stop();
+        m_Explosion.Play();
         if (boat != null)
         {
             boat.m_Rigidbody.AddExplosionForce(1000, this.transform.position, 10);
