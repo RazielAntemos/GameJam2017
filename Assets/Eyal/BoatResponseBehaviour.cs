ff632e58348e1dc8829ee66095f1cc24ca951314
eshalev <eshalev@cisco.com>
2017-01-20 20:29:10 +0100

    added bonus item and collision with ship

---
+++ Assets/Eyal/BoatResponseBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatResponseBehaviour : MonoBehaviour {
+    Rigidbody m_Rigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        m_Rigidbody = this.GetComponent<Rigidbody>();
+        m_Rigidbody.velocity = new Vector3(3, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    /// <summary>
+    /// Called when boat picks up a bonus
+    /// </summary>
+    /// <param name="bonus">the bonus that is picked up</param>
+    public void OnBonus(BonusBehaviour bonus)
+    {
+
+    }
+}
---
+++ Assets/Eyal/BonusBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBehaviour : MonoBehaviour {
+   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //check if colliding with a boat
+        var boat = collision.collider.GetComponent<BoatResponseBehaviour>();
+        Debug.Log("collision");
+        if (boat != null)
+        {
+
+            boat.OnBonus(this);
+            GameObject.Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        //check if colliding with a boat
+        var boat = collider.GetComponent<BoatResponseBehaviour>();
+        Debug.Log("trigger");
+        if (boat != null)
+        {
+
+            boat.OnBonus(this);
+            Destroy(gameObject);
+        }
+    }
+
+}
