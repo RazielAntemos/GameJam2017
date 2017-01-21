ca436fc1957dea83a7130000a7ccaaddc92e887d
eshalev <eshalev@cisco.com>
2017-01-21 18:33:08 +0100

    Added speed limit

---
+++ Assets/Eyal/assets/RigidBodySpeedLimitter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodySpeedLimitter : MonoBehaviour {
+
+    public float m_SpeedLimit = 1;
+    Rigidbody m_RigidBody;
+	// Use this for initialization
+	void Start () {
+        m_RigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_RigidBody.velocity.magnitude > m_SpeedLimit)
+        {
+            m_RigidBody.velocity=m_RigidBody.velocity.normalized * m_SpeedLimit;
+        }
+	}
+}
