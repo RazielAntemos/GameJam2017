fa2dbf8c37a65b31ef8fb3a833e3348ca93e5318
petee <petee@LAPTOP-GOP257VS>
2017-01-21 15:55:57 +0100

    WavePowerSlider

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
     [SerializeField]
     [Range(1,500)]
-    float timeLeft = 300f;
-
+    float timeLeft = 300;
+    public float minutes;
+    public float seconds;
     public Text timerText;
 
     private void Update()
     {
         timeLeft -= Time.deltaTime;
-
-        timerText.text = "Time left: " + Mathf.Round(timeLeft);
+        if (seconds < 0)
+        {
+            seconds = 60;
+            minutes--;
+        }
+        minutes = timeLeft / 60;
+        seconds = timeLeft;
+        timerText.text = "Time left: " + Mathf.Round(minutes) + - + Mathf.Round(timeLeft);
     }
 }
---
+++ Assets/Scripts/WavePowerBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WavePowerBar : MonoBehaviour {
+
+    public Slider powerSlider;
+    float wavePower;
+
+    BoatResponseBehaviour boatResponse;
+
+
+    private void Update()
+    {
+        boatResponse = GetComponent<BoatResponseBehaviour>();
+        wavePower = boatResponse._WaveStrength;
+
+        powerSlider.value = wavePower;
+        if (wavePower == 1)
+            //  powerSlider.enabled = false;
+            powerSlider.gameObject.SetActive(false);
+        else
+            powerSlider.gameObject.SetActive(true);
+
+
+        Debug.Log(powerSlider.enabled);
+    }
+}
