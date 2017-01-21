cb4a899427f64e72d07a7cd4a274d5420a0c5353
eshalev <eshalev@cisco.com>
2017-01-21 16:58:44 +0100

    Added physics based waves

---
+++ Assets/Eyal/assets/ParticleWaveCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleWaveCollider : MonoBehaviour 
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public float m_ParticleStrength = 1;
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Vector3 force = collisionEvents[i].velocity * m_ParticleStrength;
+                rb.AddForce(force);
+            }
+            i++;
+        }
+    }
+}
