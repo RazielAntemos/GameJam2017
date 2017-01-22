a4db167629985b4b209647942627a05a1787f535
eshalev <eshalev@cisco.com>
2017-01-22 16:06:53 +0100

    Addded trident

--- Assets/Eyal/assets/WaveGenerator.cs
+++ Assets/Eyal/assets/WaveGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class WaveGenerator : MonoBehaviour {
     public ParticleSystem m_Emitter;
-
+    public GameObject m_Trident;
 	// Use this for initialization
 	void Start () {
         
@@ -21,16 +21,21 @@ public class WaveGenerator : MonoBehaviour {
         if (Input.GetMouseButton(0))
         {
             calculateWavePhysics(1);
+        }else
+        {
+            m_Trident.transform.localPosition = new Vector3(0, 0, 0);
         }
 
     }
 
     private void calculateWavePhysics(float strength)
     {
+        float timeCycle = ( Mathf.Sin(Time.time * 5));
         m_Emitter.Emit(1+(int)(
-            Math.Pow((1+Math.Sin(Time.time*5)),3)
+            Math.Pow(1+timeCycle,3)
             * 200*Time.deltaTime
             ));
+        m_Trident.transform.localPosition = new Vector3(0, (Mathf.Cos(Time.time * 5))/4, 0);
         var boats = GameObject.FindObjectsOfType<BoatResponseBehaviour>();
         foreach(var boat in boats)
         {
