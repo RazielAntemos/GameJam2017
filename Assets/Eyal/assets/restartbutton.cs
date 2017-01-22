e6ea90c328d8dc2578408151bcb6415bc1029d1b
eshalev <eshalev@cisco.com>
2017-01-22 13:55:19 +0100

    fixed windows aspect ratio

---
+++ Assets/Eyal/assets/restartbutton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartbutton : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+                {
+                    if (hit.transform.name == "RestartButton")
+                    {
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
+                    }
+
+                }
+            }
+        }
+    }
+}
